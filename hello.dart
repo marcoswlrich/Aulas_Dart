18c86f7ff162d95fe496eb8a07b591d5bf507748
marcoswlrich <marcosapw.dev@gmail.com>
2022-09-01 00:35:37 -0400

    first commmit

---
+++ hello.dart
@@ -0,0 +1,25 @@
+// Operadores matematicos
+
+void main() {
+  var num1 = 10;
+  var num2 = 30;
+
+  var total = num1 + num2;
+
+  print(total);
+
+  // Concatenacao de texto
+
+  var palavras = "Hello" + "Word";
+  print(palavras);
+
+  // Estruturas de decisao
+
+  var isTrue = num1 > num2;
+
+  if (isTrue) {
+    print('Numero maior');
+  } else {
+    print('Numero menor');
+  }
+}
